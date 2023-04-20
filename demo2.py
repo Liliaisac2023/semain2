263e385d53e270e5db28eb58341c0d120f0a9462
liliaisac2023 <lilia.isac2023@gmail.com>
2023-04-20 19:53:58 -0400

    first commit

---
+++ demo2.py
@@ -0,0 +1,22 @@
+age = 21
+
+if age >=18:
+    print ("tu peux voter")
+else: 
+    print("go sleep")
+
+print("end")
+
+age = int(input("enter your age: "))
+name = input("imput entrer your name: ")
+
+if age >=18:
+    print (f"tu peux voter {name}")
+    print ("tu peux voter")
+
+else: 
+    print(f"bonjour")
+    print("Sorry!")
+    print("go sleep")
+
+print("end")
